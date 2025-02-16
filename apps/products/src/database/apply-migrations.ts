3e364fef3bb72fb6649508e735b17956b47e9949
YaroslavKharchenko0 <82085991+YaroslavKharchenko0@users.noreply.github.com>
2025-02-16 16:24:16 +0200

    feature/docker  (#5)
    * chore: add dockerfile for notifications
    
    * chore: add docker compose

--- apps/products/src/database/apply-migrations.ts
+++ apps/products/src/database/apply-migrations.ts
@@ -3,8 +3,9 @@ import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Logger } from "@nestjs/common";
+import { resolve } from "path";
 
-const migrationsPath = 'apps/products/migrations';
+const migrationsPath = resolve(__dirname, '..', 'products', 'migrations');
 
 export const applyMigrations = async (client: PoolClient) => {
   const db = drizzle(client, { schema });
